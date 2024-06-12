29c3a08bde91b139ff6e0d80a0c4dc914528d065
LukaPolo <112101327+LukaPolo@users.noreply.github.com>
2024-06-12 20:16:53 -0300

    Movimiento del jugador

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour{
+    [SerializeField]private float speed = 5;
+
+    void Start(){
+        PlayerInput.directionalInput += Move;
+    }
+
+    void Move(float xAxis, float yAxis){
+        transform.Translate(new Vector2(xAxis, yAxis) * speed * Time.deltaTime);
+    }
+}

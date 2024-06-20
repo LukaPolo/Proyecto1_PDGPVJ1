3c4416f4bdfa3c00718095c5eaf1f8054c869e32
LukaPolo <112101327+LukaPolo@users.noreply.github.com>
2024-06-19 22:01:18 -0300

    Añadido de Inputs

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -4,9 +4,21 @@ using UnityEngine;
 
 public class PlayerInput : MonoBehaviour{
     public delegate void DirectionalInput(float xAxis, float yAxis);
+    public delegate void AttackInput(bool button);
+    public delegate void DashInput(bool button);
+    public delegate void ProtectInput(bool button);
+    public delegate void InteractInput(bool button);
     public static DirectionalInput directionalInput;
+    public static AttackInput attackInput;
+    public static DashInput dashInput;
+    public static ProtectInput protectInput;
+    public static InteractInput interactInput;
 
     void Update(){
         directionalInput(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        attackInput(Input.GetButton("Attack"));
+        dashInput(Input.GetButton("Dash"));
+        protectInput(Input.GetButton("Protect"));
+        interactInput(Input.GetButton("Interact"));
     }
 }
